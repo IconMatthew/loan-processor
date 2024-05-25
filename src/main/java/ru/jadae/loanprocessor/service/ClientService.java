fcea489dd9c5bdf3f694f2eea70afe7058235549
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 01:02:40 +0300

    Created service for client

--- src/main/java/ru/jadae/loanprocessor/service/ClientService.java
+++ src/main/java/ru/jadae/loanprocessor/service/ClientService.java
@@ -1,4 +1,33 @@
 package ru.jadae.loanprocessor.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.jadae.loanprocessor.dao.ClientDAO;
+import ru.jadae.loanprocessor.dao.LoanApplicationDAO;
+import ru.jadae.loanprocessor.entities.Client;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ClientService {
+
+    @Autowired
+    private final ClientDAO clientDAO;
+
+    public void applyLoan(Client client){
+        clientDAO.save(client);
+    }
+
+    public Client getClientByIdData(String idData){
+        return clientDAO.getByIdData(idData);
+    }
+
+    public List<Client> getAllClients(){
+        return clientDAO.getAllClients();
+    }
+
+
 }
