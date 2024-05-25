4366cdac5d5ce32c12ff176980462ec747924e33
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 00:58:55 +0300

    Created methods for clientDAO

--- src/main/java/ru/jadae/loanprocessor/dao/ClientDAO.java
+++ src/main/java/ru/jadae/loanprocessor/dao/ClientDAO.java
@@ -1,7 +1,35 @@
 package ru.jadae.loanprocessor.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.jadae.loanprocessor.entities.Client;
+
+import java.util.List;
 
 @Repository
 public class ClientDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Client save(Client client){
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.persist(client);
+        session.getTransaction().commit();
+        return client;
+    }
+
+    public List<Client> getAllClients() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select c from Client c", Client.class).getResultList();
+    }
+
+    public Client getByIdData(String idData) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select c from Client c where idData like :idData", Client.class)
+                .setParameter("idData", idData).getSingleResult();
+    }
 }
