5a714a32a9c3ca9a39b44f8c12337b3bd73e8b3e
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 01:00:57 +0300

    Created entity class for loan application

--- src/main/java/ru/jadae/loanprocessor/entities/LoanApplication.java
+++ src/main/java/ru/jadae/loanprocessor/entities/LoanApplication.java
@@ -1,4 +1,36 @@
 package ru.jadae.loanprocessor.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "loanApplication")
+@Entity
 public class LoanApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @Column(name="status")
+    private Boolean status;
+
+    @Column(name="term")
+    @Size(min=1, max = 360)
+    private Integer term;
+
+    @Column(name = "approvedAmount", precision = 19, scale = 2)
+    private BigDecimal approvedAmount;
+
+    @OneToOne(mappedBy = "loanApplication", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @ToString.Exclude
+    private LoanContract loanContract;
 }
