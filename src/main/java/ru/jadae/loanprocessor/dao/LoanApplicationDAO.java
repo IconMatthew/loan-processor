bef2490dd2533973569bc8a73286d71db45cb46b
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 01:00:29 +0300

    Created DAO for loan application

--- src/main/java/ru/jadae/loanprocessor/dao/LoanApplicationDAO.java
+++ src/main/java/ru/jadae/loanprocessor/dao/LoanApplicationDAO.java
@@ -1,12 +1,25 @@
 package ru.jadae.loanprocessor.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.jadae.loanprocessor.entities.Client;
+import ru.jadae.loanprocessor.entities.LoanApplication;
+
+import java.math.BigDecimal;
 
 @Repository
 public class LoanApplicationDAO {
 
-//    @Autowired
-//    private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public LoanApplication saveApplication(LoanApplication loanApplication){
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.persist(loanApplication);
+        session.getTransaction().commit();
+        return loanApplication;
+    }
 }
