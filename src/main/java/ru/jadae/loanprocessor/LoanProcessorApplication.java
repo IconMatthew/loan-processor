28b36fbfbe9d645b24bc636cf5bd9fe11b7d46f4
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 22:17:46 +0300

    Added bean for validation annotations post-processing

--- src/main/java/ru/jadae/loanprocessor/LoanProcessorApplication.java
+++ src/main/java/ru/jadae/loanprocessor/LoanProcessorApplication.java
@@ -2,12 +2,19 @@ package ru.jadae.loanprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 public class LoanProcessorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoanProcessorApplication.class, args);
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
     }
 
 }
