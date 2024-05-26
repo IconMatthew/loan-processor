96f6dc3e222677b4f146caab4f6ccd105298cea0
IconMatthew <jadaevmatvei@gmail.com>
2024-05-26 22:14:43 +0300

    Added validation for fields

--- src/main/java/ru/jadae/loanprocessor/entities/Client.java
+++ src/main/java/ru/jadae/loanprocessor/entities/Client.java
@@ -1,6 +1,7 @@
 package ru.jadae.loanprocessor.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.ToString;
 import ru.jadae.loanprocessor.enums.Gender;
@@ -21,23 +22,26 @@ public class Client {
     @Column(name="fullName", length = 150, nullable = false)
     private String fullName;
 
-    @Column(name="idData", unique = true, nullable = false)
+    @Column(name="idData", nullable = false)
+    @Pattern(regexp = "\\d{4} \\d{6}", message = "Паспортные данные должны быть записаны в формате: 'ssss nnnnnn'")
     private String idData;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "gender")
+    @Column(name = "gender", nullable = false)
     private Gender gender;
 
-    @Column(name = "address")
+    @Column(name = "address", nullable = false)
     private String address;
 
     @Column(name = "phoneNumber", nullable = false)
+    @Pattern(regexp = "\\+\\d{1,3}\\d{1,4}\\d{7,10}", message = "Номер телефона должен быть записан в формате: '+CCCNNNNXXXXXXX'")
     private String phoneNumber;
 
-    @Column(name = "workingExperience")
+
+    @Column(name = "workingExperience", nullable = false)
     private String workingExperience;
 
-    @Column(name = "loanAmount", precision = 19, scale = 2)
+    @Column(name = "loanAmount", precision = 19, scale = 2, nullable = false)
     private BigDecimal loanAmount;
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
